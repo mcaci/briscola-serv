266a036d240169abb7c29f1ec3d889efea46c39d
mcaci <michele.caci@gmail.com>
2022-04-14 09:05:42 +0200

    Remove unused client code in the server

--- daemon/endpoint/grpc.go
+++ daemon/endpoint/grpc.go
@@ -6,11 +6,6 @@ import (
 	"github.com/mcaci/briscola-serv/pb"
 )
 
-func PointsRequestEncodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
-	req := r.(PointsRequest)
-	return &pb.CardPointsRequest{CardNumber: req.CardNumber}, nil
-}
-
 func PointsRequestDecodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
 	req := r.(*pb.CardPointsRequest)
 	return PointsRequest{CardNumber: req.CardNumber}, nil
@@ -21,16 +16,6 @@ func PointsResponseEncodeGRPC(ctx context.Context, r interface{}) (interface{}, 
 	return &pb.CardPointsResponse{Points: res.Points}, nil
 }
 
-func PointsResponseDecodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
-	res := r.(*pb.CardPointsResponse)
-	return PointsResponse{Points: res.Points, Err: ""}, nil
-}
-
-func CountRequestEncodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
-	req := r.(CountRequest)
-	return &pb.PointCountRequest{CardNumber: req.CardNumbers}, nil
-}
-
 func CountRequestDecodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
 	req := r.(*pb.PointCountRequest)
 	return CountRequest{CardNumbers: req.CardNumber}, nil
@@ -39,18 +24,6 @@ func CountRequestDecodeGRPC(ctx context.Context, r interface{}) (interface{}, er
 func CountResponseEncodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
 	res := r.(CountResponse)
 	return &pb.PointCountResponse{Count: res.Points}, nil
-}
-
-func CountResponseDecodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
-	res := r.(*pb.PointCountResponse)
-	return CountResponse{Points: res.Count, Err: ""}, nil
-}
-
-func CompareRequestEncodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
-	req := r.(CompareRequest)
-	first := &pb.ItalianCard{Number: req.FirstCardNumber, Seed: pb.Seed(req.FirstCardSeed)}
-	second := &pb.ItalianCard{Number: req.SecondCardNumber, Seed: pb.Seed(req.SecondCardSeed)}
-	return &pb.CardCompareRequest{FirstCard: first, SecondCard: second, Briscola: pb.Seed(req.BriscolaSeed)}, nil
 }
 
 func CompareRequestDecodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
@@ -66,9 +39,4 @@ func CompareRequestDecodeGRPC(ctx context.Context, r interface{}) (interface{}, 
 func CompareResponseEncodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
 	res := r.(CompareResponse)
 	return &pb.CardCompareResponse{SecondCardWinsOverFirstOne: res.SecondCardWins}, nil
-}
-
-func CompareResponseDecodeGRPC(ctx context.Context, r interface{}) (interface{}, error) {
-	res := r.(*pb.CardCompareResponse)
-	return CompareResponse{SecondCardWins: res.SecondCardWinsOverFirstOne, Err: ""}, nil
 }
