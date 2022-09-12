4da005d1d368c4a3344eafb7862d2ecd17bca8fe
mcaci <michele.caci@gmail.com>
2022-09-12 09:16:54 +0200

    Add contour ingress and update metallb deployment

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,12 @@
 kind create cluster --config ./deployment/kind/conf.yaml
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+helm install metallb metallb/metallb
 helm install briscola-serv ./deployment/briscola-serv
+
+# install doesn't work at first try
+# helm upgrade briscola-serv ./deployment/briscola-serv
+
+# istio
 # kubectl label namespace default istio-injection=enabled --overwrite
 
